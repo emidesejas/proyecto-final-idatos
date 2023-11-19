54a8d076453a14cb4193532f4e51e9a110821656
Emiliano <edesejas@eagerworks.com>
2023-11-19 11:43:08 -0300

    chore: setup supabase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,30 @@
+import {
+  type Session,
+  createPagesBrowserClient,
+} from "@supabase/auth-helpers-nextjs";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 import { type AppType } from "next/app";
 
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
+import { useState } from "react";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp: AppType<{ initialSession: Session }> = ({
+  Component,
+  pageProps,
+}) => {
+  // Create a new supabase browser client on every first render.
+  const [supabaseClient] = useState(() => createPagesBrowserClient());
+
+  return (
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
