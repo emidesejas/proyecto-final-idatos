d773222734d1c91b3c36a7cddfb0bcfbb79e624e
Emiliano <edesejas@eagerworks.com>
2023-11-20 00:32:19 -0300

    chore: update aliases

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
-import { helloRouter } from "~/server/api/routers/hello";
-import { createTRPCRouter } from "~/server/api/trpc";
+import { helloRouter } from "@server/api/routers/hello";
+import { createTRPCRouter } from "@server/api/trpc";
 
 /**
  * This is the primary router for your server.
