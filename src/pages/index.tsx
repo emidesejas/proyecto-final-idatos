896a5fa34415df64f1e30d862e6e32a7aeb6df74
Emiliano <edesejas@eagerworks.com>
2023-11-23 20:42:01 -0300

    feat: spotify credits in search bar and animate loading state

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,17 @@ import Image from "next/image";
 import { api } from "@utils/api";
 import { Input } from "@components/ui/input";
 import { useDebounce } from "@hooks/useDebounce";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { ScrollArea } from "@components/ui/scroll-area";
 import { Button } from "@components/ui/button";
 import { useSupabaseClient } from "@supabase/auth-helpers-react";
 import { useToast } from "@components/ui/use-toast";
 import { useRouter } from "next/router";
+import { TrackCard } from "@components/TrackCard";
+import SpotifyLogo from "@images/spotify.png";
+import { X } from "lucide-react";
+
+import { Bars } from "react-loader-spinner";
 
 export default function Home() {
   const [queryString, setQueryString] = useState("");
@@ -18,7 +23,9 @@ export default function Home() {
   const { toast } = useToast();
   const router = useRouter();
 
-  const { data } = api.spotify.search.useQuery(
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const { data, isFetching } = api.spotify.search.useQuery(
     { text: query },
     {
       enabled: !!query,
@@ -26,6 +33,7 @@ export default function Home() {
       keepPreviousData: !!query,
     },
   );
+
   return (
     <>
       <Head>
@@ -65,37 +73,44 @@ export default function Home() {
         </div>
         <h1 className="mt-32 text-5xl font-semibold">Conocé tu música</h1>
 
-        <Input
-          className="mt-9 max-w-lg text-xl"
-          onChange={(event) => {
-            setQueryString(event.target.value);
-          }}
-          placeholder="Buscá tu canción favorita..."
-        />
-        <ScrollArea className="mt-10 w-full flex-1 px-8">
-          {data?.tracks.items.map(
-            ({
-              id,
-              name,
-              artists: [{ name: artistName }],
-              album: {
-                images: [{ url }],
-              },
-            }) => (
-              <div key={id} className="mt-2 flex h-12 flex-row gap-2">
-                <img
-                  className="aspect-square h-full rounded object-cover"
-                  src={url}
-                  alt="pepito"
-                />
-                {/* <Image src={url} alt={"pepito"} height={20} width={20} /> */}
-                <div className="flex flex-col">
-                  <h2 className="font-bold">{name}</h2>
-                  <h3 className="font-light">{artistName}</h3>
-                </div>
-              </div>
-            ),
+        <div className="mt-9 flex w-full max-w-lg flex-row items-center">
+          <Input
+            ref={inputRef}
+            className="text-xl"
+            onChange={(event) => {
+              setQueryString(event.target.value);
+            }}
+            placeholder="Buscá tu canción favorita..."
+          />
+          {isFetching ? (
+            <Bars width={32} height={32} color="" wrapperClass="ml-2" />
+          ) : (
+            <X
+              className="ml-2 h-8 w-8 cursor-pointer"
+              onClick={() => {
+                if (inputRef.current) inputRef.current.value = "";
+                setQueryString("");
+              }}
+            />
           )}
+        </div>
+
+        <div className="mt-2 flex flex-row items-center justify-center">
+          <h3 className="text-sm font-light">
+            Resultados obtenidos a través de
+          </h3>
+          <Image
+            src={SpotifyLogo}
+            alt={"Spotify Logo"}
+            height={20}
+            objectFit="contain"
+            className="ml-2"
+          />
+        </div>
+        <ScrollArea className="mt-4 w-full max-w-xl flex-1 px-3">
+          {data?.tracks.items.map((track) => (
+            <TrackCard track={track} key={track.id} />
+          ))}
         </ScrollArea>
       </main>
     </>
